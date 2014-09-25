054106d0a04d7ed2935488dc8932c2fc103befaa
Christian Vuerings <vueringschristian@gmail.com>
2014-09-24 18:07:23 -0700

    Make it truly open

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ app.use(express.static('public'));
 
 app.all('*', function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'X-Requested-With');
   next();
 });
 
